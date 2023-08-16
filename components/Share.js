4620b63c9ddb2483991848cb1b8feccb33ac0236
jalal1985khan <jalal1985khan@gmail.com>
2023-08-17 00:08:02 +0530

    Added share with dynamic path

--- components/Share.js
+++ components/Share.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import { TwitterShareButton,FacebookShareButton, WhatsappShareButton  } from 'react-share';
-import { AiFillTwitterSquare,AiFillFacebook,AiOutlineWhatsApp  } from "react-icons/ai";
+import { AiFillTwitterSquare, AiFillFacebook, AiOutlineWhatsApp } from "react-icons/ai";
+import { usePathname } from 'next/navigation'
 
 
 function Share() {
+  const pathname = usePathname()
+  const murl = `https://upfront.global/${pathname}`
   return (
 <>
 <Container className="d-flex flex-column">
-              <p>Share This Page</p>
+              <p>Share This Page </p>
 <TwitterShareButton 
-url={`https://upfront.global/why-well-being/`}
+url={murl}
 title="Check out this iHateReading custom repository "
 className="my-1 s-button btn btn-primary px-2"
 >
@@ -18,7 +21,7 @@ className="my-1 s-button btn btn-primary px-2"
 </TwitterShareButton>
 
 <FacebookShareButton 
-url={`https://upfront.global/why-well-being/`}
+url={murl}
           title="Check out this iHateReading custom repository "
           className="my-1 s-button btn btn-primary px-2"
 >
@@ -26,11 +29,11 @@ url={`https://upfront.global/why-well-being/`}
 </FacebookShareButton>              
 
 <WhatsappShareButton 
-url={`https://upfront.global/why-well-being/`}
+url={murl}
           title="Check out this iHateReading custom repository "
           className="my-1 s-button btn btn-primary px-2"
 >
-<AiOutlineWhatsApp  size={28} /> Facebook                                      
+<AiOutlineWhatsApp  size={28} /> Whatsapp                                      
 </WhatsappShareButton>                      
 </Container>
 <Container className="d-flex flex-column mt-5 mb-5">
